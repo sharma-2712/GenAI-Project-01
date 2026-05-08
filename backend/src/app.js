9aee5ad1ccaa37c8876234aeacd1bf8e3aac3b3b
sharma-2712 <atul.9369sharma@gmail.com>
2026-05-08 18:15:51 +0530

    CORS updated in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const app=express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173","https://prep-iq-tau.vercel.app/"],
     credentials:true
 }))
 // Require all the Routes
