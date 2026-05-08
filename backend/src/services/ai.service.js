58d34052de044c4a83d10231f6b71cc0545dc18f
sharma-2712 <atul.9369sharma@gmail.com>
2026-05-08 18:51:47 +0530

    fixed puppeteer render config

--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -101,7 +101,10 @@ Return only JSON.
 
 
 async function generatePdfFromHtml(htmlContent){
-    const browser=await puppeteer.launch();
+    const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+});
     const page=await browser.newPage();
     await page.setContent(htmlContent,{waitUntil:"networkidle0"})
     const pdfBuffer=await page.pdf({
