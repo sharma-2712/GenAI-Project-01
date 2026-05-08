b2ce7e42f93ce668ce8c3127a3ec0138d8fa29e8
sharma-2712 <atul.9369sharma@gmail.com>
2026-05-08 19:01:37 +0530

    fixed puppeteer for render deployment

--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -1,7 +1,8 @@
 const {GoogleGenAI}=require("@google/genai")
 const {z}=require("zod")
 const {zodToJsonSchema}=require("zod-to-json-schema")
-const puppeteer=require('puppeteer')
+const chromium = require("@sparticuz/chromium");
+const puppeteer = require("puppeteer-core");
 
 
 const ai = new GoogleGenAI({
@@ -102,8 +103,10 @@ Return only JSON.
 
 async function generatePdfFromHtml(htmlContent){
     const browser = await puppeteer.launch({
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath(),
+    headless: chromium.headless,
 });
     const page=await browser.newPage();
     await page.setContent(htmlContent,{waitUntil:"networkidle0"})
