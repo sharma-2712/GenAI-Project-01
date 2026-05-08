04d3cfa09d711da7e9761ffe9074c374e6d3bfd3
sharma-2712 <atul.9369sharma@gmail.com>
2026-05-08 18:01:52 +0530

    cookies update for deployment

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -41,7 +41,12 @@ async function registerUserController(req,res){
         username:user.username
     },process.env.JWT_SECERT,{expiresIn:"1d"})
 
-    res.cookie("token",token);
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000
+});
 
     res.status(201).json({
         message:"User Registered Successfully",
@@ -85,7 +90,12 @@ async function loginUserController(req,res) {
         username:user.username
     },process.env.JWT_SECERT,{expiresIn:"1d"})
 
-    res.cookie("token",token)
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000
+});
 
     res.status(201).json({
         message:"User LoggedIn Successfully",
@@ -110,7 +120,11 @@ async function logoutUserController(req,res) {
         await tokenBlacklistModel.create({token})
     }
 
-    res.clearCookie("token")
+    res.clearCookie("token", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+});
     
     res.status(200).json({
         message:"User Logged-Out Successfully",
